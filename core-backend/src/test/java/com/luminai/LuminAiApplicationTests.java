ebaea9e06e97b12eb32cfd6b1c1e8f4ed36b24b2
Virtus Dakura <dakuravirtus@gmail.com>
2026-07-14 00:32:01 +0000

    feat: fix github test failing

--- core-backend/src/test/java/com/luminai/LuminAiApplicationTests.java
+++ core-backend/src/test/java/com/luminai/LuminAiApplicationTests.java
@@ -2,8 +2,14 @@ package com.luminai;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ActiveProfiles;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest
+@ActiveProfiles("test")
+@Import(TestcontainersConfig.class)
+@Testcontainers(disabledWithoutDocker = true)
 class LuminAiApplicationTests {
 
   @Test
