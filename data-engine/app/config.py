248fe0724a5c006e94b93e6c82983e498914d217
Virtus Dakura <dakuravirtus@gmail.com>
2026-08-03 12:39:42 +0000

    fix: wrong link from docs

--- data-engine/app/config.py
+++ data-engine/app/config.py
@@ -23,6 +23,7 @@ class Settings(BaseSettings):
     app_version: str = "0.1.0"
     app_host: str = "0.0.0.0"
     app_port: int = 8000
+    app_frontend_url: str = "https://luminai-sand.vercel.app"
     debug: bool = False
 
     # CORS Configuration
